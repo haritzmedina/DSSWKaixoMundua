a328fc802660413b27e1fe0f16fdd31c2ebe9dc9
Haritz Medina <haritzmedina@gmail.com>
2015-12-07 17:17:16 +0100

    Register form validation on server. Users saved. Added minor changes

---
+++ database.py
@@ -0,0 +1,67 @@
+# Libraries
+from google.appengine.ext import ndb
+import hashlib
+
+
+user_key = ndb.Key('User', 'default_user')
+
+
+# Data model
+class User(ndb.Model):
+    name = ndb.TextProperty(indexed=True)
+    password = ndb.TextProperty()
+    email = ndb.TextProperty(indexed=True)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    photo = ndb.BlobProperty()
+
+
+# Manages users: create, delete, select, modify
+class UserManager:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create(username, password, email, photo):
+        user = User(parent=user_key)
+
+        user.name = username
+        user.password = hashlib.md5(password).hexdigest()
+        user.email = email
+        user.photo = photo
+
+        user.put()
+        return True;
+
+    @staticmethod
+    def select():
+        users = ndb.gql(
+            'SELECT * '
+            'FROM User '
+            'WHERE ANCESTOR IS :1 '
+            'ORDER BY date DESC',
+            user_key
+        )
+
+        return users.get()
+
+    @staticmethod
+    def select_by_username(username):
+        user = ndb.gql(
+            'SELECT * '
+            'FROM User '
+            'WHERE name = :1 '
+            'ORDER BY date DESC',
+            username
+        )
+        return user.get()
+
+    @staticmethod
+    def select_by_email(email):
+        user = ndb.gql(
+            'SELECT * '
+            'FROM User '
+            'WHERE email = :1 '
+            'ORDER BY date DESC',
+            email
+        )
+        return user.get()
--- kaixomundua.py
+++ kaixomundua.py
@@ -18,10 +18,16 @@ import webapp2
 # Jinja templates
 import os
 import jinja2
+# Regular expression
+import re
 from google.appengine.ext import vendor
 
+# Add language compatibility
 vendor.add('lib')
 from webapp2_extras import i18n
+from webapp2_extras.i18n import gettext as _
+# Add database file
+import database
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -31,36 +37,83 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 JINJA_ENVIRONMENT.install_gettext_translations(i18n)
 
 
+# Register page backend
 class Register(webapp2.RequestHandler):
     def get(self):
-        Language().setLanguage(self.request.get('language'))
+        Language.setlanguage(self.request.get('language'))
         template = JINJA_ENVIRONMENT.get_template('static/templates/register.html')
         self.response.write(template.render())
 
     def post(self):
-        # TODO check variables with python RE
-        self.response.write("Not working yet")
+        # Retrieve request data
+        username = self.request.get('username')
+        password1 = self.request.get('password1')
+        password2 = self.request.get('password2')
+        email = self.request.get('email')
+
+        # Load success and fail templates
+        registerTemplate = JINJA_ENVIRONMENT.get_template('static/templates/register.html')
+        registeredTemplate = JINJA_ENVIRONMENT.get_template('static/templates/registered.html')
+
+        # Check email is well formed
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            self.response.write(registerTemplate.render(error=_("Bad email.")))
+            return None
+        # Check passwords min size is 6
+        if len(password1) < 6:
+            self.response.write(registerTemplate.render(error=_("Password min length not reached.")))
+            return None
+        # Check passwords match
+        if password1 != password2:
+            self.response.write(registerTemplate.render(error=_("Passwords do not match.")))
+            return None
+        # Check user exists
+        user = database.UserManager.select_by_username(username)
+        if user is not None:
+            self.response.write(registerTemplate.render(error=_("Username exists")))
+            return None
+        # Check email exists
+        user = database.UserManager.select_by_email(email)
+        if user is not None:
+            self.response.write(registerTemplate.render(error=_("Email exists")))
+            return None
+
+        # Save in DB
+        if database.UserManager.create(username, password1, email, None):
+            self.response.write(registeredTemplate.render(username=username))
 
 
+# Welcome page backend
 class Welcome(webapp2.RequestHandler):
     def get(self):
-        Language().setLanguage(self.request.get('language'))
+        Language.setlanguage(self.request.get('language'))
         template = JINJA_ENVIRONMENT.get_template('static/templates/welcome.html')
         self.response.write(template.render())
 
 
+# Users manage handler
+class UsersPage(webapp2.RequestHandler):
+    def get(self):
+        Language.setlanguage(self.request.get('language'))
+        # TODO Retrieve users
+        users = database.UserManager.select()
+        # TODO Render template
+        template = JINJA_ENVIRONMENT.get_template('static/templates/users.html')
+        self.response.write(template.render(users=users))
+
+
+# i18n language handler
 class Language:
     def __init__(self):
         pass
 
-    def setLanguage(self, lang):
+    @staticmethod
+    def setlanguage(lang):
         i18n.get_i18n().set_locale(lang)
 
 
 app = webapp2.WSGIApplication([
     ('/', Welcome),
     ('/register', Register),
-    ('/agurtu', Welcome),
-    ('/saludo', Welcome),
-    ('/greeting', Welcome)
+    ('/users', UsersPage)
 ], debug=True)
