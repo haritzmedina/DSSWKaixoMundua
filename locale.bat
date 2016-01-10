8bd9359a21b8677d9639e47fb05779d51e178b47
Haritz Medina <haritzmedina@gmail.com>
2016-01-10 23:11:24 +0100

    Babel script minor changes

--- locale.bat
+++ locale.bat
@@ -1,7 +1,7 @@
-D:\Python27\Scripts\pybabel.exe extract -F .\locale\babel.cfg -o ./locale/messages.pot .
+pybabel extract -F .\locale\babel.cfg -o ./locale/messages.pot .
 
-D:\Python27\Scripts\pybabel.exe update -l eu_ES -d ./locale -i ./locale/messages.pot
-D:\Python27\Scripts\pybabel.exe update -l es_ES -d ./locale -i ./locale/messages.pot
-D:\Python27\Scripts\pybabel.exe update -l en_US -d ./locale -i ./locale/messages.pot
+pybabel update -l eu_ES -d ./locale -i ./locale/messages.pot
+pybabel update -l es_ES -d ./locale -i ./locale/messages.pot
+pybabel update -l en_US -d ./locale -i ./locale/messages.pot
 
-D:\Python27\Scripts\pybabel.exe compile -f -d ./locale
+pybabel compile -f -d ./locale
